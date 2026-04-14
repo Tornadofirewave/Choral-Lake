0b4df3836c539dc7c4fbfeb89e12b0b5dec71cf4
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2026-04-14 11:51:33 -0700

    feat: added animated water tilemap + bobber sprite + workshopping cursor change mechanic

--- Assets/Scripts/Fishing/FishingCusor.cs
+++ Assets/Scripts/Fishing/FishingCusor.cs
@@ -4,6 +4,15 @@ public class FishingCursor : MonoBehaviour
 {
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
-    
-    
+    public Vector2 hotSpot = Vector2.zero;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, hotSpot, cursorMode);
+    }
 }  
