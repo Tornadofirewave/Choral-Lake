e2808c6476b7691a461f19da4ca9317b73bd4ab5
Tornadofirewave <142632652+Tornadofirewave@users.noreply.github.com>
2026-04-29 22:10:55 -0700

    added fishing rod pickup

---
+++ Assets/Scripts/Gameplay/FishingRodPickup.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using ChoralLake.Audio;
+using ChoralLake.Core;
+using ChoralLake.Data;
+using ChoralLake.UI;
+
+namespace ChoralLake.Gameplay
+{
+    public class FishingRodPickup : MonoBehaviour, IInteractable, IPointerClickHandler
+    {
+        [SerializeField] private FishingRodSO rod;
+        [SerializeField] private string sfxId = "sfx_pickup_rod";
+        [SerializeField] private string interactPrompt = "Pick up rod";
+
+        private bool collected;
+
+        public string InteractPrompt => interactPrompt;
+        public bool CanInteract => rod != null && !collected;
+        public Transform Transform => transform;
+
+        public void Interact() => Collect();
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (ModalStack.AnyOpen || ModalStack.JustClosed) return;
+            Collect();
+        }
+
+        private void Collect()
+        {
+            if (rod == null || collected) return;
+            collected = true;
+
+            var gm = GameManager.Instance;
+            gm.GrantRod(rod.Id);
+            gm.EquipRod(rod.Id);
+            AudioManager.Instance?.PlaySfx(sfxId);
+            RewardPopup.Instance?.Show(rod.InventoryIcon, rod.DisplayName);
+            gm.SaveGame();
+
+            Destroy(gameObject);
+        }
+    }
+}
